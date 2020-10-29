c3e94142698286f45be112df855e81ed9772a827
dongjeremy <dongjeremy2020@163.com>
2020-10-29 20:16:26 +0800

    Initial commit

---
+++ src/main/java/org/cloud/panzer/PanzerApplication.java
@@ -0,0 +1,13 @@
+package org.cloud.panzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PanzerApplication.class, args);
+    }
+
+}
